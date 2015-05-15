84abc6beff596ee6791852c702f9abe1e671f4c4
Ostap Horbach <ostap.horbach@admins-Mac-mini-2.local>
2015-05-15 11:40:19 +0300

    Initial Commit

---
+++ Thing3Tests/Thing3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Thing3Tests.swift
+//  Thing3Tests
+//
+//  Created by Ostap Horbach on 5/15/15.
+//  Copyright (c) 2015 Ostap Horbach. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Thing3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
